c6053154681220842757144f7429d13921d74ae4
Perazzolo Sean <sean.perazzolo@gmail.com>
2026-04-28 09:28:49 +0200

    del typos

--- projects/LCS/LCS/Games/SolutionGroupMagicSqare.lean
+++
@@ -1,8 +0,0 @@
-import LCS.Games.MagicSquare.SolutionGroup
-
-/-!
-# Magic Square Solution Group
-
-This module is kept as a compatibility import for the old misspelled module name.
-Use `LCS.Games.MagicSquare.SolutionGroup` instead.
--/
