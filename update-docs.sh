4d86c12602fafb6e3c1b42ad9a62ddfb280550e2
Perazzolo Sean <sean.perazzolo@gmail.com>
2026-06-11 11:26:41 +0200

    update-docs for new website

--- update-docs.sh
+++ update-docs.sh
@@ -1,51 +1,31 @@
 #!/bin/bash
 
 BASE_DIR=$(pwd)
+# 1. Build doc4
+echo "Building doc4"
+cd "$BASE_DIR/src/docbuild"
+lake build LCS:docs
+cp -r .lake/build/doc "$BASE_DIR/postprocess_docs/source"
+cd "$BASE_DIR/postprocess_docs"
+.venv/bin/python3 main.py
+cp -r source/doc/. "$BASE_DIR/docs/documentation"
 
-# 1. Generate the docs
-# echo "Generating manual"
-# cd "$BASE_DIR/projects/docs_LCS"
-# lake exe generate-docs
-#
-# cp -r _out/html-multi/* "$BASE_DIR/docs/"
-#
-
-# read -p "Do you want to generate the Interactive Source? (it's quite long) [y/N]: " response
-# if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-# then
-	# 2. Generate the interactive source
-# 	echo "Generating Interactive Source"
-# 	cd "$BASE_DIR/src"
-# 	lake build LCS:literate
-# 	lake exe verso-html .lake/build/literate ../../docs/source
-# else 
-# 	echo "Skipping Interactive Source generation."
-# fi
-
-
-# echo "Done! Docs are updated."
-
-# 3. Build doc4
-read -p "Do you want to build doc4  [y/N]: " doc_response
-if [[ "$doc_response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-	echo "Building doc4"
-	cd "$BASE_DIR/src/docbuild"
-	lake build LCS:docs
-	cp -r .lake/build/doc "$BASE_DIR/postprocess_docs/source"
-	cd "$BASE_DIR/postprocess_docs"
-	.venv/bin/python3 main.py
-	cp -r source/doc/. "$BASE_DIR/docs/documentation"
-fi
-
-# 4. Compile status.typ
+# 2. Compile.typ
 echo "Compiling status report"
 cd "$BASE_DIR/typst"
 typst compile status.typ
 mv status.pdf "$BASE_DIR/docs/status.pdf"
+cd "$BASE_DIR/report"
+typst compile main.typ
+mv main.pdf "$BASE_DIR/docs/report.pdf"
 
 
-# Cleanup (remove source files)
+# 3. Take the source files to the docs folder for reference
+cp "$BASE_DIR/postprocess_docs/source/index.html" "$BASE_DIR/docs/index.html"
+cp "$BASE_DIR/postprocess_docs/source/CNAME" "$BASE_DIR/docs/CNAME"
+
+
+# 4. Cleanup (remove source files)
 rm -rf "$BASE_DIR/postprocess_docs/source/doc"/*
 rm -rf "$BASE_DIR/src/docbuild/.lake/build/doc"*
 
